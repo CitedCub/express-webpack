21e698b5cc8cb037cac74f0a2223bec2b5d1f481
andreas.erlandsson <andreas.erlandsson@gmail.com>
2020-03-11 14:18:26 +0100

    Renders client-side

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import path from "path";
-import express from "express";
+var path = require("path");
+var express = require("express");
 
 const app = express(),
-  DIST_DIR = __dirname,
+  DIST_DIR = path.join(__dirname, "dist");
   HTML_FILE = path.join(DIST_DIR, "index.html");
 
+console.log("__dirname:", DIST_DIR);
+
+app.use(express.static(DIST_DIR));
 // Send index.html on every GET request
 app.get("/*", (req, res) => {
+  console.log("Request:", req.url);
   res.sendFile(HTML_FILE);
 });
 
-app.use(express.static(DIST_DIR));
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App X listening to ${PORT}....`);
+  console.log(`App listening to ${PORT}....`);
   console.log("Press Ctrl+C to quit.");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,21 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    server: "./server.js"
+    bundle: "./src/index.js",
+    // server: "./server.js"
   },
   output: {
     path: path.join(__dirname, "dist"),
     publicPath: "/",
     filename: "[name].js"
   },
-  target: "node",
-  node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false, // if you don't put this is, __dirname
-    __filename: false // and __filename return blank or /
-  },
-  externals: [nodeExternals()], // Need this to avoid error when working with express
+  // target: "node",
+  // node: {
+  //   // Need this when working with express, otherwise the build fails
+  //   __dirname: false, // if you don't put this is, __dirname
+  //   __filename: false // and __filename return blank or /
+  // },
+  // externals: [nodeExternals()], // Need this to avoid error when working with express
   module: {
     rules: [
       {
@@ -30,18 +31,26 @@ module.exports = {
         }
       },
       {
-        // Loads the javascript into html template provided
-        // Entry point is set below in HtmlWebPackPlugin in Plugins
-        test: /\.html$/,
-        use: [{ loader: "html-loader" }]
-      }
+        // Transpiles ES6-8 into ES5
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      // {
+      //   // Loads the javascript into html template provided
+      //   // Entry point is set below in HtmlWebPackPlugin in Plugins
+      //   test: /\.html$/,
+      //   use: [{ loader: "html-loader" }]
+      // }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html",
-      excludeChunks: ["server"]
+      template: "./src/index.html",
+      // filename: "./index.html",
+      // excludeChunks: ["server"]
     })
   ]
 };
